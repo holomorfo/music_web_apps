4b412679178aa7142c18fe69879922c70838e0de
DESKTOP-U3ABQ9C\crist <holomorfo.mx@gmail.com>
2018-03-08 13:29:38 -0600

    Node config and pange manager

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // server
 var express = require('express');
+var path = require('path');
+
 var app = express();
 app.set('port', process.env.PORT || 5000);
+app.use(express.static('public'));
+app.use(express.static('public/plr_app'));
+app.use(express.static('public/piano_dynamics_app'));
 
 // Our first route
 app.get('/', function (req, res) {
   //res.send('app_files/index.html');
-  res.sendFile(path.join(__dirname + 'app_files/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Listen to port
